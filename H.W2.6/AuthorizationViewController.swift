8ddbd426fcb5297abf391f3c9e65342678959661
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-02-15 20:43:55 +0300

    bag fix

--- H.W2.6/AuthorizationViewController.swift
+++ H.W2.6/AuthorizationViewController.swift
@@ -20,7 +20,7 @@ class AuthorizationViewController: UIViewController {
     private let login = "1"
     private let password = "1"
     
-    // MARK: - Override Fanction
+    // MARK: - Override Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -43,39 +43,37 @@ class AuthorizationViewController: UIViewController {
     
     // MARK: - IBActions
     @IBAction func actionLogOutButton() {
-        guard nameTextField.text != login || passwordTextField.text != password else { return }
-        toAlertController(massage: "Вы ввели неверный логин или пароль") { _ in
-            self.passwordTextField.text = nil
+        if nameTextField.text != login || passwordTextField.text != password {
+            toAlertController(massage: "Вы ввели не верный логин или пароль")
+            passwordTextField.text = nil
+        } else {
+            performSegue(withIdentifier: "go", sender: nil)
         }
     }
     
     
-    @IBAction func actionForgotUserNameButton() {
-        toAlertController(massage: "User name \(login)")
-    }
-    
-    @IBAction func actionForgotPasswordButton() {
-        toAlertController(massage: "Pasword \(password)")
+    @IBAction func actionForgotUserNameAndPasswordButton(_ sender: UIButton) {
+        sender.tag == 0
+        ? toAlertController(massage: "My login \(login)")
+        : toAlertController(massage: "My password \(password)")
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
-        guard segue.source is PersonViewController else { return }
         nameTextField.text = nil
         passwordTextField.text = nil
     }
     
     // MARK: - Private Functions
     
-    private func toAlertController(massage: String, closure: ((UIAlertAction) -> Void)? = nil) {
+    private func toAlertController(massage: String) {
         let alert = UIAlertController(
             title: "Внимание",
             message: massage,
             preferredStyle: .alert
         )
-        let actionAlert = UIAlertAction(title: "OK", style: .default, handler: closure)
+        let actionAlert = UIAlertAction(title: "OK", style: .default)
         alert.addAction(actionAlert)
         present(alert, animated: true)
-        
     }
      
 }
@@ -84,7 +82,6 @@ class AuthorizationViewController: UIViewController {
 extension AuthorizationViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
         if textField == nameTextField {
             passwordTextField.becomeFirstResponder()
         } else {
